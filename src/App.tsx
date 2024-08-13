14b262b523ec962592c789e38c912db614a0fcc7
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-08-14 00:27:03 +0200

    useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,46 @@
 import { useRef } from "react";
 import Button from "./components/Button";
 import Input from "./components/Input";
-import Container from "./components/Container";
-import Form from "./components/Form";
+//import Container from "./components/Container";
+import Form, { type FormHandle } from "./components/Form";
 
 function App() {
-
-  const nameInputRef = useRef<HTMLInputElement>(null);
+  const customFormRef = useRef<FormHandle>(null);
 
   // Here in the parent component we DO KNOW the shape of the data that will be passed to the onSave function,
   // because wr know which fields are in the form.
   function handleSave(data: unknown) {
     const extractedData = data as { surname: string, age: string };
-    console.log(extractedData);
+    console.log('extractedData: ', extractedData);
+    customFormRef.current?.clear();
   }
 
   return (
     <main>
-      <Input
-        id="name"
-        type="text"
-        label="Your Name"
-        ref={nameInputRef}
-      />
       <Form onSave={handleSave}>
-        <Input id="surname" type="text" label="Your Surname" />
+        <Input id="name" type="text" label="Your Name" />
         <Input id="age" type="number" label="Your Age" />
         <p>
           <Button type="submit">Save</Button>
         </p>
       </Form>
-      <Container
-        as={Button}
-        onClick={() => alert("Container clicked!")}
-        type="button"
-      >
-        Click me!
-      </Container>
+      {/** 
+      <p>
+        <Container
+          as={Button}
+          onClick={() => alert("Container clicked!")}
+          type="button"
+        >
+          Click me!
+        </Container>
+      </p>
       <p>
         <Button onClick={() => alert("Button clicked!")}>A Button</Button>
       </p>
       <p>
         <Button href="https://reactjs.org">A Link</Button>
       </p>
+      */}
     </main>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,10 @@
-import { type ComponentPropsWithoutRef, type FormEvent } from "react";
+import {
+    type ComponentPropsWithoutRef,
+    type FormEvent,
+    forwardRef,
+    useImperativeHandle,
+    useRef
+} from "react";
 
 // Which tupe should be passed to the onSave function?
 // Form component is a wrapper component, so we don't know what exactly kind of data it will receive.
@@ -7,22 +13,48 @@ type FormProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...props }: FormProps) {
+// The type for object that will be returned by the useImperativeHandle hook.
+// It is also a type for the ref object that will be passed to the Form component in forwardRef.
+export type FormHandle = {
+    clear: () => void;
+};
+
+// forwardRef is a generic function, that needs 2 types to be passed:
+// 1. Type which will be passed to the ref
+// 2. Type of the props that will be passed to the component function
+const Form = forwardRef<FormHandle, FormProps>(function Form({ onSave, children, ...props }, ref) {
+
+    const formRef = useRef<HTMLFormElement>(null);
+
+    // useImpreativeHandle is a hook that allows us to access the ref of the child component from the parent component.
+    useImperativeHandle(ref, () => {
+        // Here we can define the methods that we want to be accessible from the parent component.
+        return {
+            clear() {
+                console.log('Clearing the form');
+                formRef.current?.reset();
+            }
+        };
+    });
+
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        console.log('Form submitted');
 
         const formData = new FormData(event.currentTarget);
         const data = Object.fromEntries(formData.entries());
         onSave(data);
+        formRef.current?.reset();
     }
 
     return (
         <form
             {...props}
             onSubmit={handleSubmit}
+            ref={formRef}
         >
             {children}
         </form>
     );
-}
+});
+
+export default Form;
