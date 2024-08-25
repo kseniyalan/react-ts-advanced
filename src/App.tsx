43d4148bf7d61c2d090fea36179d195b24f2ddeb
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-08-25 23:28:54 +0200

    Add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,16 @@
-import { useRef } from "react";
-import Button from "./components/Button";
-import Input from "./components/Input";
-//import Container from "./components/Container";
-import Form, { type FormHandle } from "./components/Form";
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-  const customFormRef = useRef<FormHandle>(null);
-
-  // Here in the parent component we DO KNOW the shape of the data that will be passed to the onSave function,
-  // because wr know which fields are in the form.
-  function handleSave(data: unknown) {
-    const extractedData = data as { surname: string, age: string };
-    console.log('extractedData: ', extractedData);
-    customFormRef.current?.clear();
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave}>
-        <Input id="name" type="text" label="Your Name" />
-        <Input id="age" type="number" label="Your Age" />
-        <p>
-          <Button type="submit">Save</Button>
-        </p>
-      </Form>
-      {/** 
-      <p>
-        <Container
-          as={Button}
-          onClick={() => alert("Container clicked!")}
-          type="button"
-        >
-          Click me!
-        </Container>
-      </p>
-      <p>
-        <Button onClick={() => alert("Button clicked!")}>A Button</Button>
-      </p>
-      <p>
-        <Button href="https://reactjs.org">A Link</Button>
-      </p>
-      */}
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
